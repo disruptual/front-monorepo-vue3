71477605d35b94fff70147c36c92404d43789671
vincentvermeersch <sk8_vince@hotmail.fr>
2021-11-09 17:32:32 +0100

    - Add item-details view

--- apps/back/src/pages/admin/item-details/index.vue
+++ apps/back/src/pages/admin/item-details/index.vue
@@ -17,6 +17,8 @@ const query = useItemApi().findByIdQuery(props.id, {
   relations: []
 });
 
+console.log("query items ===> ", query.data);
+
 const breadCrumbLabel = computed(
   () => query.data.value?.title ?? t('breadcrumb.itemDetail')
 );
@@ -28,5 +30,86 @@ useBreadCrumbs(breadCrumbLabel);
     <dsp-center>
       <h2>{{ item.title }}</h2>
     </dsp-center>
+    <dsp-container>
+      <dsp-surface>
+        <dsp-container is-small>
+          <dsp-swiper as="ul">
+            <dsp-swiper-item v-for="media in item.medias" :key="media.id" as="li">
+              <dsp-image class="photo" :src="media.thumbnails?.avatar" />
+            </dsp-swiper-item>
+          </dsp-swiper>
+        </dsp-container>
+        <dsp-container is-small>
+          <div class="color">
+            <strong>Couleurs :</strong>
+            <dsp-flex>
+              <div
+                v-for="color in item.colors"
+                :key="color.id"
+                class="color-preview"
+                :style="{ '--color': '#' + color.hex }"
+              />
+            </dsp-flex>
+          </div>
+
+          <div v-if="item.category" class="category">
+            <strong>Catégorie :</strong>
+            <span>{{ item.category.name }}</span>
+          </div>
+
+          <div v-if="item.condition" class="state">
+            <strong>Etat :</strong>
+            <!-- <span>{{ item.condition.stateName }}</span> -->
+          </div>
+
+          <div v-if="item.size" class="size">
+            <strong>Taille :</strong>
+            <span>{{ item.size.name }}</span>
+          </div>
+
+          <div v-if="item.packageDelivery" class="package-delivery">
+            <strong>Format de colis :</strong>
+            <span>{{ item.packageDelivery.name }}</span>
+          </div>
+
+          <div v-if="item.brand" class="brand">
+            <strong>Marque :</strong>
+            <span>{{ item.brand.name }}</span>
+          </div>
+
+          <div v-if="item.deliveries" class="deliveries">
+            <strong>Modes de livraison :</strong>
+            <span>{{ item.deliveriesString }}</span>
+          </div>
+
+          <div v-if="item.composition" class="composition">
+            <strong>Composition :</strong>
+            <span>{{ item.composition }}</span>
+          </div>
+
+          <div v-if="item.content" class="content">
+            <strong>Description:</strong>
+            <span>{{ item.content }}</span>
+          </div>
+
+          <div v-if="item.locations" class="locations">
+            <strong>Magasins de dépôt:</strong>
+            <ul class="location-list">
+              <li v-for="location in item.locations" :key="location.id">{{ location.name }}</li>
+            </ul>
+          </div>
+        </dsp-container>
+      </dsp-surface>
+    </dsp-container>
   </dsp-query-loader>
 </template>
+
+<style lang="scss">
+.color-preview {
+  width: var(--spacing-lg);
+  height: var(--spacing-lg);
+  background-color: var(--color);
+  margin-right: 0.5rem;
+  border: solid 1px #aaa;
+}
+</style>
