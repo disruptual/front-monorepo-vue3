1b0690adf51919861d33722697021480d2e852f8
loicpennequin <lo.pennequin@gmail.com>
2022-02-14 15:54:30 +0100

    fix dsp-select tests

--- libs/ui/src/components/select/index.test.js
+++ libs/ui/src/components/select/index.test.js
@@ -1,59 +1,51 @@
 import { mount } from '@vue/test-utils';
 import DspSelect from './index.vue';
 
-const valuesTest = [
-  { id: 'test1', name: 'test1' },
-  { id: 'test2', name: 'test2' },
-  { id: 'test3', name: 'test3' },
-  { id: 'test4', name: 'test4' }
+const options = [
+  { label: 'test1', value: 'test1' },
+  { label: 'test2', value: 'test2' },
+  { label: 'test3', value: 'test3' },
+  { label: 'test4', value: 'test4' }
 ];
 
 describe('DspSelect', () => {
   it('should render without throwing', () => {
     expect(() =>
       mount(DspSelect, {
-        props: { modelValue: null, options: valuesTest }
+        props: { modelValue: null, options: options }
       })
     ).not.toThrow();
   });
 
   describe('emit', () => {
-    it('should emit update:modelValue with third option value', async () => {
-      const expectedId2 = valuesTest[2].id;
+    it('should emit update:modelValue with selected option value', async () => {
       const wrapper = mount(DspSelect, {
-        props: { modelValue: null, options: valuesTest }
+        props: { modelValue: null, options: options }
       });
 
-      const option = wrapper.findAll('option')[2];
+      let option = wrapper.findAll('option')[3];
       await option.setSelected();
 
       expect(wrapper.emitted()).toHaveProperty('update:modelValue');
-      expect(wrapper.emitted()['update:modelValue'][0][0].id).toEqual(
-        expectedId2
+      expect(wrapper.emitted()['update:modelValue'][0][0]).toEqual(
+        options[2].value
       );
-    });
 
-    it('should emit update:modelValue with second option value', async () => {
-      const expectedId2 = valuesTest[1].id;
-      const wrapper = mount(DspSelect, {
-        props: { modelValue: null, options: valuesTest }
-      });
-
-      const secondOption = wrapper.findAll('option')[1];
-      await secondOption.setSelected();
+      option = wrapper.findAll('option')[2];
+      await option.setSelected();
 
       expect(wrapper.emitted()).toHaveProperty('update:modelValue');
-      expect(wrapper.emitted()['update:modelValue'][0][0].id).toEqual(
-        expectedId2
+      expect(wrapper.emitted()['update:modelValue'][1][0]).toEqual(
+        options[1].value
       );
     });
   });
 
   describe('props', () => {
     it('should render multiple values', async () => {
-      const expectedIds = [valuesTest[1].id, valuesTest[2].id];
+      const expectedIds = [options[1].id, options[2].id];
       const wrapper = mount(DspSelect, {
-        props: { modelValue: null, options: valuesTest, multiple: true }
+        props: { modelValue: null, options: options, multiple: true }
       });
 
       const secondOption = wrapper.findAll('option')[1];
