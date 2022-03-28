f36684a3306fc0be0d5354550bb3c7d4f555469a
etienneftn <etienne@disruptual.com>
2022-03-28 10:45:05 +0200

    remove comment

--- apps/back/src/components/order-history/index.vue
+++ apps/back/src/components/order-history/index.vue
@@ -112,7 +112,6 @@ const getStepTranslationVariables = step => {
           <span class="button-label">{{ t('orderHistory.next') }}</span>
         </dsp-loading-button>
       </dsp-flex>
-      <!-- <pre>{{ order.history }}</pre> -->
       <div class="step__description">{{ getStepDescription(step) }}</div>
     </li>
   </ul>
