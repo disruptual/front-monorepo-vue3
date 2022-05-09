defe28cbbe47ef2ec524e4f2db1d2f88ad3bd31d
loicpennequin <lo.pennequin@gmail.com>
2022-05-09 09:40:40 +0200

    sign up form wip

--- apps/front/src/components/user/sign-up-form/index.vue
+++ apps/front/src/components/user/sign-up-form/index.vue
@@ -3,9 +3,11 @@ export default { name: 'SignUpForm' };
 </script>
 
 <script setup>
+import { unref } from 'vue';
 import { useForm } from '@dsp/ui';
 import { useRoute } from 'vue-router';
 import { USER_GENDERS } from '@dsp/business';
+import { isDefined } from '@dsp/core';
 
 const form = useForm({
   onSubmit(values) {
@@ -18,6 +20,43 @@ const route = useRoute();
 const genders = [
   { label: 'Madame', value: USER_GENDERS.FEMALE },
   { label: 'Monsieur', value: USER_GENDERS.MALE }
+];
+
+const passwordValidators = [
+  {
+    name: 'containsNumber',
+    message: 'form.errors.containsNumber',
+    handler(value) {
+      return !isDefined(value) || /[0-9]/.test(value);
+    }
+  },
+  {
+    name: 'containsUppercase',
+    message: 'form.errors.containsUppercase',
+    handler(value) {
+      return !isDefined(value) || /[A-Z]/.test(value);
+    }
+  },
+  {
+    name: 'containsLowercase',
+    message: 'form.errors.containsLowercase',
+    handler(value) {
+      return !isDefined(value) || /[a-z]/.test(value);
+    }
+  }
+];
+
+const passwordConfirmValidators = [
+  {
+    name: 'passwordMatch',
+    message: 'form.errors.passwordMatch',
+    handler(value, { formContext }) {
+      const { values } = unref(formContext);
+      if (!values.plainPassword) return true;
+
+      return value === values.value.plainPassword;
+    }
+  }
 ];
 </script>
 
@@ -77,6 +116,40 @@ const genders = [
         label="Adresse e-mail"
       />
     </dsp-smart-form-field>
+
+    <dsp-smart-form-field
+      v-slot="slotProps"
+      name="plainPassword"
+      :minlength="8"
+      :validators="passwordValidators"
+      required
+    >
+      <dsp-form-control
+        v-slot="formProps"
+        v-model="slotProps.field.value"
+        v-bind="slotProps"
+        label="Mot de passe"
+      >
+        <dsp-input-password v-bind="formProps" />
+      </dsp-form-control>
+    </dsp-smart-form-field>
+
+    <dsp-smart-form-field
+      v-slot="slotProps"
+      name="confirmedPassword"
+      :validators="passwordValidators"
+      required
+    >
+      <dsp-form-control
+        v-slot="formProps"
+        v-model="slotProps.field.value"
+        v-bind="slotProps"
+        label="Confirmez le mot de passe"
+      >
+        <dsp-input-password v-bind="formProps" />
+      </dsp-form-control>
+    </dsp-smart-form-field>
+
     <dsp-smart-form-submit is-full-width>Suivant</dsp-smart-form-submit>
   </dsp-smart-form>
 </template>
