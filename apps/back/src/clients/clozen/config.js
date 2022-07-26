4d807c3d028fd1f3403740a00cf4204f2c4f361c
loicpennequin <lo.pennequin@gmail.com>
2022-07-26 16:26:35 +0200

    feat(back): setup team for Clozen

--- apps/back/src/clients/clozen/config.js
+++ apps/back/src/clients/clozen/config.js
@@ -3,5 +3,7 @@ import defaultConfig from '../disruptual/config';
 import logo from 'client/assets/logo.svg?url';
 
 export default merge({}, defaultConfig, {
-  logo
+  logo,
+
+  DefaultLayoutHeader: { isLogoDisplayed: true }
 });
--- apps/back/src/clients/clozen/theme.js
+++ apps/back/src/clients/clozen/theme.js
@@ -1,3 +1,18 @@
 import theme from '@dsp/default-theme';
+import { merge } from 'lodash-es';
+import HelveticaNeueRegular from './assets/fonts/HelveticaNeue_regular.ttf';
+import HelveticaNeueBold from './assets/fonts/HelveticaNeue_bold.ttf';
 
-export default theme;
+export default merge({}, theme, {
+  fonts: {
+    body: {
+      400: HelveticaNeueRegular,
+      700: HelveticaNeueBold
+    }
+  },
+  palettes: {
+    brand: {
+      baseColor: '#F4A650'
+    }
+  }
+});
